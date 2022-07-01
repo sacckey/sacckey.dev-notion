3e55b20795db8837fbd62afee30682e189353bed
sacckey <sackeyad24@gmail.com>
2022-07-01 20:33:47 +0900

    add notionhq

---
+++ lib/notion.ts
@@ -0,0 +1,91 @@
+import { Client } from "@notionhq/client"
+import { QueryDatabaseResponse, ListBlockChildrenResponse } from '@notionhq/client/build/src/api-endpoints.d'
+
+export type Page = Extract<QueryDatabaseResponse['results'][number], {properties: unknown}>
+export type Block = Extract<ListBlockChildrenResponse['results'][number], {has_children: unknown}> & {
+  children?: Block[],
+  brothers?: Block[]
+}
+export type RichText = Extract<Page['properties']['Name'], {title: unknown}>['title'][number]
+
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+})
+
+export const getDatabase = async (): Promise<Page[]> => {
+  const pages: Page[] = []
+  const databaseId = process.env.NOTION_DATABASE_ID || ''
+  const { results } = await notion.databases.query({
+    database_id: databaseId,
+  })
+
+  for (const page of results) if ('properties' in page) pages.push(page)
+
+  return pages
+}
+
+export const getPage = async (pageId: string): Promise<Page> => {
+  const response = await notion.pages.retrieve({ page_id: pageId })
+
+  if ('properties' in response) return response
+
+  throw new Error('properties is not in response')
+}
+
+export const getBlock = async (blockId: string): Promise<Block> => {
+  const response = await notion.blocks.retrieve({ block_id: blockId })
+
+  if ('has_children' in response) return response
+
+  throw new Error('has_children is not in response')
+}
+
+export const getChildBlocks = async (blockId: string): Promise<Block[]> => {
+  const blocks: Block[] = []
+  let cursor
+  while (true) {
+    const { results, next_cursor }: { results: ListBlockChildrenResponse['results'], next_cursor: string | null } = await notion.blocks.children.list({
+      start_cursor: cursor,
+      block_id: blockId,
+    })
+
+    for(const block of results) if ('has_children' in block) blocks.push(block)
+
+    if (!next_cursor) break
+
+    cursor = next_cursor
+  }
+
+  return blocks
+}
+
+export const getAllChildBlocks = async (block: Block): Promise<Block[]> => {
+  const childBlocks: Block[] = await getChildBlocks(block.id)
+  const mergedChildBlocks: Block[] = []
+
+  for (let i = 0; i < childBlocks.length; i++) {
+    const childBlock = childBlocks[i]
+
+    if (childBlock.has_children) {
+      childBlock.children = await getAllChildBlocks(childBlock)
+    }
+
+    if (i===0) {
+      mergedChildBlocks.push(childBlock)
+      continue
+    }
+
+    const firstChildBlock = mergedChildBlocks[mergedChildBlocks.length - 1]
+    if (!firstChildBlock) throw new Error('failed to fetch child blocks')
+
+    if (firstChildBlock.type === childBlock.type && (firstChildBlock.type === "bulleted_list_item" || firstChildBlock.type === "numbered_list_item")){
+      firstChildBlock.brothers = firstChildBlock.brothers || []
+      firstChildBlock.brothers.push(childBlock)
+    }
+    else {
+      mergedChildBlocks.push(childBlock)
+    }
+  }
+
+  return mergedChildBlocks
+}
---
+++ pages/[id].tsx
@@ -0,0 +1,188 @@
+import { RichText, Page, Block, getBlock, getAllChildBlocks, getPage } from "../lib/notion"
+import Head from 'next/head'
+import styles from "./post.module.css"
+import Link from "next/link"
+import { Fragment } from "react"
+
+export default function Post({ page, blocks }: { page: Page, blocks: Block[] }) {
+  const title = 'title' in page.properties.Name ? page.properties.Name.title : null
+
+  return (
+    <div>
+      <Head>
+        {title && (<title>{title[0]?.plain_text}</title>)}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <article className={styles.container}>
+        <h1 className={styles.name}>
+          {title && (<Text texts={title} />)}
+        </h1>
+        <section>
+          {blocks.map((block) => (
+            <Fragment key={block.id}>{renderBlock(block)}</Fragment>
+          ))}
+          <Link href="/">
+            <a className={styles.back}>← Go home</a>
+          </Link>
+        </section>
+      </article>
+    </div>
+  )
+}
+
+export const Text = ({ texts }: { texts: RichText[] }) => {
+  return (
+    <>
+      {
+        texts.map((value, index) => {
+          const { plain_text, href, annotations: { bold, code, color, italic, strikethrough, underline } } = value
+
+          return (
+            <span
+              key={index}
+              className={[
+                bold ? styles.bold : "",
+                code ? styles.code : "",
+                italic ? styles.italic : "",
+                strikethrough ? styles.strikethrough : "",
+                underline ? styles.underline : "",
+              ].join(" ")}
+              style={color !== "default" ? { color } : {}}
+            >
+              {href ? <a href={href}>{plain_text}</a> : plain_text}
+            </span>
+          )
+        })
+      }
+    </>
+  )
+}
+
+const renderBlock = (block: Block) => {
+  const { id, type } = block
+
+  switch (type) {
+    case "paragraph":
+      return (
+        <p>
+          <Text texts={block[type].rich_text} />
+        </p>
+      )
+    case "heading_1":
+      return (
+        <h1>
+          <Text texts={block[type].rich_text} />
+        </h1>
+      )
+    case "heading_2":
+      return (
+        <h2>
+          <Text texts={block[type].rich_text} />
+        </h2>
+      )
+    case "heading_3":
+      return (
+        <h3>
+          <Text texts={block[type].rich_text} />
+        </h3>
+      )
+    case "bulleted_list_item":
+      return (
+        <ul>
+          <li><Text texts={block[type].rich_text} /></li>
+          {block.children?.map((child) => <Fragment key={child.id}>{renderBlock(child)}</Fragment>)}
+          {block.brothers?.map((brother) =>
+            <Fragment key={brother.id}>
+              {type in brother && (<li><Text texts={brother[type].rich_text} /></li>)}
+              {brother.children?.map((child) => <Fragment key={child.id}>{renderBlock(child)}</Fragment>)}
+            </Fragment>
+          )}
+        </ul>
+      )
+    case "numbered_list_item":
+      return (
+        <ol>
+          <li><Text texts={block[type].rich_text} /></li>
+          {block.children?.map((child) => <Fragment key={child.id}>{renderBlock(child)}</Fragment>)}
+          {block.brothers?.map((brother) =>
+            <Fragment key={brother.id}>
+              {type in brother && (<li><Text texts={brother[type].rich_text} /></li>)}
+              {brother.children?.map((child) => <Fragment key={child.id}>{renderBlock(child)}</Fragment>)}
+            </Fragment>
+          )}
+        </ol>
+      )
+    case "to_do":
+      return (
+        <div>
+          <label htmlFor={id}>
+            <input type="checkbox" id={id} defaultChecked={block[type].checked} />{" "}
+            <Text texts={block[type].rich_text} />
+          </label>
+        </div>
+      )
+    case "toggle":
+      return (
+        <details>
+          <summary>
+            <Text texts={block[type].rich_text} />
+          </summary>
+          {block.children?.map((child) => (
+            <Fragment key={child.id}>{renderBlock(child)}</Fragment>
+          ))}
+        </details>
+      )
+    case "child_page":
+      return <p>{block[type].title}</p>
+    case "divider":
+      return <hr key={id} />
+    case "quote":
+      return <blockquote key={id}>{block[type].rich_text[0].plain_text}</blockquote>
+    case "code":
+      return (
+        <pre className={styles.pre}>
+          <code className={styles.code_block} key={id}>
+            {block[type].rich_text[0].plain_text}
+          </code>
+        </pre>
+      )
+    case "bookmark":
+      const href = block[type].url
+      return (
+        <a href={ href } target="_brank" className={styles.bookmark}>
+          { href }
+        </a>
+      )
+    default:
+      return `❌ Unsupported block (${
+        type === "unsupported" ? "unsupported by Notion API" : type
+      })`
+    }
+  }
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps = async (context: { params: { id: string } }) => {
+  try {
+    const { id } = context.params
+    const page = await getPage(id)
+    const block = await getBlock(id)
+    const blocks = await getAllChildBlocks(block)
+
+    return {
+      props: {
+        page,
+        blocks,
+      },
+      revalidate: 60 * 10,
+    }
+  } catch (e) {
+    throw new Error(`Failed to fetch post, received status ${e}`)
+  }
+}
